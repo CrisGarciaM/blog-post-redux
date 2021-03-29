ee30b134657fff932b07342f7fa6488771efdaed
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-03-29 09:30:41 +0200

    Combined Reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
+import postReducer from './postReducer';
+import userReducer from './userReducer';
 
 export default combineReducers({
-  replaceMe: () => "Hello",
+  posts: postReducer,
+  users: userReducer,
 });
